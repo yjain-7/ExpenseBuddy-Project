9d17d2a7d016cc988dd3db73e180f19fa9b633c8
yjain-7 <jainyash4292@gmail.com>
2024-08-09 18:13:50 +0530

    Config update for reload issue

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    historyApiFallback: true,
+  }
 })
